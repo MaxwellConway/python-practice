05802ae7e3201456e1ea9de2c26000dc448d5b00
MaxwellConway <maxconway23@gmail.com>
2023-07-06 14:19:24 -0400

    more examples

---
+++ app.py
@@ -0,0 +1,103 @@
+# age = 20
+# price = 19.95
+# first_name = "Max"
+# is_online = True
+# print(age)
+
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+# birth_year = input("Enter your birth year: ")
+# age = 2023 - int(birth_year)
+# print(age)
+# int()
+# float()
+# bool()
+# str()
+
+# First = 10.1
+# Second = 20
+# Sum = First + Second
+# print(Sum)
+
+# first = float(input("First: "))
+# second = float(input("Second: "))
+# sum = first + second
+# print("Sum is " + str(sum))
+
+# course = 'Python for Beginners'
+# print('Python' in course)
+
+# course = 'Python for Beginners'
+# print(course.replace('for', '4'))
+
+# x = 10
+# x = x + 3
+# x += 3
+# x *= 3
+
+# x = (10 + 3) * 2
+# print(x)
+
+# x = 3 > 2
+# x = 3 >= 2
+# x = 3 < 2
+# x = 3 <= 2
+# x = 3 == 2
+# x = 3 != 2
+# print(x)
+
+# price = 25
+# print(price > 10 and price < 30)
+# print(price > 26 and price < 26)
+# print(price > 26 or price < 26)
+
+# price = 5
+# print(not price > 4)
+# print(not price > 10)
+
+# temperature = 9
+# if temperature > 30:
+#     print("It's a hot day.")
+#     print("Drink plenty of water.")
+# elif temperature > 20:  # (20, 30]
+#     print("It's a nice day.")
+# elif temperature > 10:  # (10, 20]
+#     print("It's a bit cold.")
+# else:
+#     print("It's cold.")
+# print("Done")
+
+
+# weight = int(input("Weight: "))
+# unit = input("(K)g or (L)bs: ")
+# if unit.upper() == "K":
+#     print("Weight in Lbs: " + str(weight * 2.205))
+# elif unit.upper() == "L":
+#     print("Weight in Kgs: " + str(weight / 2.205))
+# print("Done.")
+
+# i = 1
+# while i <= 10:
+#     print(i * '*')
+#     i = i + 1
+
+
+# 1
+# 1.1
+# True
+# 'a'
+
+# names = ["John", "Bob", "Mary", "Chris", "Max"]
+# names[0] = "Jon"
+# print(names[0:3])
+# print(names)
+
+numbers = [1, 2, 3, 4, 5]
+for item in numbers:
+    print(item)
+
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i = i + 1
